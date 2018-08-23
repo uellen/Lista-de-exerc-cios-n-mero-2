a8af60e2bf4b73395fe6f8833aad2a5c22c68b01
Uellen Nicolete <31323969+uellen@users.noreply.github.com>
2018-08-23 15:07:48 -0300

    Update q9_l3.cpp
    Correção da questão 9 da lista 3 da matéria de Linguagem de programação da ECT-UFRN

--- Ex. 03/q9_l3.cpp
+++ Ex. 03/q9_l3.cpp
@@ -4,22 +4,17 @@ using namespace std;
 int main (){
 
     int x , y;
-    int maximo = 0;
-    int maximo1= 0;
+    int mdc;
+    int i;
 
-    for (int i = 0 ; i < x ; i++){
-        if (x % i == 0 && i > maximo){
-            maximo = i;
+    cin >> x >> y;
+
+    for ( i = 1; i <= x ; i++){
+        if (x % i == 0 && y % i == 0){
+            mdc = i;
         }
     }
-    for (int i = 0 ; i < y ; i++){
-        if (y % i == 0 && i > maximo1){
-            maximo1 = i;
-        }
-    }
-    if( maximo == maximo1){
+    cout << "O mdc entre " << x << " e " << y << " eh : "<<mdc;
 
-    }
-
-
+    return 0;
 }
